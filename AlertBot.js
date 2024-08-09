0275dd887c6c18d45ed7ebbb542beb6c9ef5425a
Jonathan Bolton <jboltle@Jonathans-MacBook-Pro.local>
2024-08-09 18:22:20 -0500

    deploying on heroku using cli" "

--- AlertBot.js
+++ AlertBot.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config()
+
 // Configuration
-const token = process.env.BOT_API_KEY; // Replace with your Telegram bot token
-const chatID = process.env.CHANNEL_ID; // Replace with your Telegram bot token 
-const coinId = 'solana'; // Replace with the specific Solana coin ID (e.g., 'solana')
-const priceThreshold = 100; // Replace with your target price
+const token = process.env.BOT_API_KEY; // Telegram bot token
+const chatId = process.env.CHAT_ID; // Telegram chat ID
+const coinId = 'solana'; // Specific Solana coin ID
+const priceThreshold = 100; // Target price
 
 // Initialize Telegram bot
 const bot = new TelegramBot(token, { polling: true });
@@ -29,4 +30,3 @@ const checkPrice = async () => {
 
 // Periodically check the price (e.g., every minute)
 setInterval(checkPrice, 60000); // Check every 60 seconds
-    
